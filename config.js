8289db23e6364584b2e6ed649b59cfff1527f6ce
nguyennhannhooo <106739820+nguyennhannhooo@users.noreply.github.com>
2022-06-02 17:54:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Cậu thích tớ đúng hongg",
+    introTitle: 'Trương Gia Hân',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
